497260136ca1c2b2926e053d65dcbdb3e713fc15
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-25 00:18:34 +0300

    init
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ src/main/java/com/example/staticsitegenerator/StaticSiteGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.staticsitegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticSiteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticSiteGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/staticsitegenerator/StaticSiteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staticsitegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticSiteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
